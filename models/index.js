ae63f22e56ad5b39139bff1a09985fcbc2f2d936
udayasree006 <sathikelaudayasree@gmail.com>
2026-09-14 22:41:26 +0530

    Use Render DATABASE_URL for production

--- models/index.js
+++ models/index.js
@@ -4,13 +4,29 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
+
 const config = require(__dirname + "/../config/config.json")[env];
+
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
